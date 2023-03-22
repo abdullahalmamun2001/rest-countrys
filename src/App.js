a02012fc57fa185904ad3a1b59251665cb59cfa7
abdul1ah <almamun01817@gmail.com>
2023-03-22 08:42:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RestCountries></RestCountries>
+     {/* <ExternalUsers></ExternalUsers> */}
     </div>
   );
 }
+
+function RestCountries(){
+  const [countrys,setCountrys]=useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>setCountrys(data))
+    
+  },[])
+
+  countrys.map(country=><li>{country.length}</li>)
+  return(
+    <div>
+      {
+        countrys.map(country=><Country name={country.name} flag={country.flags} population={country.population
+        }></Country>)
+      }
+    </div>
+  )
+}
+function Country(props){
+  
+  return(
+    <div className='country'>
+      
+      <h1>{props.name.common}</h1>
+      <h2>Population :{props.population
+}</h2>
+     
+      <img src={props.flag.png} alt="" />
+    </div>
+  )
+}
+// function ExternalUsers(){
+//   const [user,setUsers]=useState([])
+//   useEffect(()=>{
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(res=>res.json())
+//     .then(data=>setUsers(data))
+//   },[])
+//   user.map(user1=><li>{user1}</li>)
+//   return(
+//     <div>
+//       <h1>User External</h1>
+//       <h1>{user.length}</h1>
+//       {
+//         user.map(users=><Users name={users.name} email={users.email}></Users>)
+//       }
+//     </div>
+//   )
+// }
+// function Users(props){
+//   return(
+//     <div className='conatiner'>
+//       <p> <strong>User Name:</strong> {props.name}</p>
+//       <p>User Email : {props.email}</p>
+//     </div>
+//   )
+// }
+
 
 export default App;
